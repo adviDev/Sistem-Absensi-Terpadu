f1202d37a34fd7025ab48b67ee5966d9f711e781
adviDev <adytyarealme@gmail.com>
2025-12-26 14:26:47 +0700

    menambahkan dao login

---
+++ SIstem_Absensi_Terpadu/src/dao/UserDAO.java
@@ -0,0 +1,31 @@
+package dao;
+import model.users;
+import config.koneksi;
+import java.sql.*;
+public class UserDAO {
+    
+    public users login(String username, String password){
+        users user = null;
+        
+        String sql = "select * from users where username=? and password=? and is_active=1";
+        try (Connection conn = koneksi.getConnection();
+                PreparedStatement ps = conn.prepareStatement(sql)){
+            ps.setString(1, username);
+            ps.setString(2, password);
+            
+            ResultSet rs = ps.executeQuery();
+            
+            if (rs.next()) {
+                user = new users();
+                user.setIdUser(rs.getInt("id_user"));
+                user.setUsername(rs.getString("username"));
+                user.setRole(rs.getString("role"));
+                user.setIsActive(rs.getBoolean("is_active"));
+            }
+            
+        } catch (SQLException e) {
+            System.out.println("error login : "+e.getMessage());
+        }
+        return user;
+    }
+}
