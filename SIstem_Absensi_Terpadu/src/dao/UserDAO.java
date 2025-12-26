b873158e4b41a25e1ce04f245158301569fd14c0
adviDev <adytyarealme@gmail.com>
2025-12-26 14:39:09 +0700

    menambahkan dao register

--- SIstem_Absensi_Terpadu/src/dao/UserDAO.java
+++ SIstem_Absensi_Terpadu/src/dao/UserDAO.java
@@ -3,7 +3,10 @@ import model.users;
 import config.koneksi;
 import java.sql.*;
 public class UserDAO {
-    
+    /**
+     * fungsi untuk cek login
+     * mengembalikan objek user jika sukses, null jika gagal
+     */
     public users login(String username, String password){
         users user = null;
         
@@ -27,5 +30,22 @@ public class UserDAO {
             System.out.println("error login : "+e.getMessage());
         }
         return user;
+    }
+    /**
+     * fungsi untuk registrasi user baru     
+     */    
+    public boolean register(String username, String password, String role){
+        String sql = "insert into users(username,password,role,is_active) values (?,?,?,1)";
+        try (Connection conn = koneksi.getConnection();
+                PreparedStatement ps = conn.prepareStatement(sql)){
+            ps.setString(1, username);
+            ps.setString(2, password);
+            ps.setString(3, role);
+            int result = ps.executeUpdate();
+            return result>0;
+        } catch (SQLException e) {
+            System.out.println("error register: "+e.getMessage());
+            return false;
+        }
     }
 }
